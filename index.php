96adf1239b9acfba63069583c1823dba01621954
Alok kumar <72178871+alok9038@users.noreply.github.com>
2021-01-13 15:59:15 +0530

    complete

---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Online Gas Booking System</title>
+     <link rel="stylesheet" href="style/bootstrap.css">
+</head>
+<body>
+   <style>
+     
+       .abc{
+           background-image: url(image/bg2.jpg);
+           background-size: cover;
+           background-repeat: no-repeat;
+           height: 500px;
+           width: 100%;
+           background-color: black;
+           margin-top: -70px;
+       }
+    </style>
+    <nav class="navbar navbar-expand lg navbar-dark ">
+    <a href="index.php" class="navbar-brand "><h5 class="font-weight-light h3">OGBS</h5></a>
+   <ul class="navbar-nav ml-auto ">
+       <li class="nav-item"><a href="" class="nav-link ">Home</a></li>
+       <li class="nav-item"><a href="" class="nav-link text-light">About OBGS</a></li>
+       <li class="nav-item"><a href="" class="nav-link text-light">Contact Us</a></li>
+       
+       <li class="nav-item"><a href="" class="nav-link text-light">Emergency No</a></li>
+       <li class="nav-item"><a href="admin/login.php" target="_blank" class="nav-link text-light">Admin Login</a></li>
+       <li class="nav-item"><a href="user/login.php" target="_blank" class="nav-link text-light">User login</a></li>
+   </ul>
+    </nav>
+   <div class="abc ">
+        <div class=" container-fluid  h-100 w-100  " style="background-color:rgba(0, 0, 0, 0.62);  padding-top:230px;;">
+<div class="card col-lg-2 mx-auto p-0 col-sm-2 col-md-2" style="background-color:rgba(0, 0, 0, 0);">
+        <a href="" class="btn btn-outline-light btn-block py-3">Book Now</a>
+</div>
+                </div>
+   </div>
+</body>
+</html>
